04436181a694164111958589a224c496e00a343b
Jimmy Chan <110892162+Shiron9586@users.noreply.github.com>
2022-09-14 15:49:36 +0800

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+#pragma warning(disable:4996)
+
+void main() {
+	char msg[] = "Cry OR Try OR Die Trying";
+	int count = 0;
+
+	for (int i = 0; i < 100; i++) {
+		if (msg[i] == 'r' || msg[i]=='R')
+		count++;
+		if (msg[i] == '\0')
+			break;
+	}
+	printf("The number of character 'r' is > %d", count);
+
+}
