7d83ed87943a8f9a65052e1814382bc6ad48775f
Jimmy Chan <110892162+Shiron9586@users.noreply.github.com>
2022-09-20 10:24:43 +0800

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -9,7 +9,8 @@ void main() {
 	char msg[] = "Cry OR Try OR Die Trying";
 	int count = 0;
 
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 100; i++) //also can use strlen(msg) for the condition
+	{
 		if (msg[i] == 'r' || msg[i]=='R')
 		count++;
 		if (msg[i] == '\0')
